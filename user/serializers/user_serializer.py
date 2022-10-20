915dc582713bb82a4e78fdc71b0d6f81660b5735
Nikita Zaitsev <nikitazv90@gmail.com>
2022-10-20 18:18:55 +0300

    upload images to aws s3

--- user/serializers/user_serializer.py
+++ user/serializers/user_serializer.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.hashers import make_password
-from rest_framework.fields import CharField
+from rest_framework.fields import CharField, ImageField
 from rest_framework.serializers import ModelSerializer
 
+from innotter import settings
+from services.upload_to_s3_user import upload_to_s3
 from user.models import User
 
 
@@ -11,11 +13,24 @@ class UserSerializer(ModelSerializer):
         required=True,
         style={'input_type': 'password', 'placeholder': 'Password'}
     )
+    image_s3_path = ImageField()
 
     class Meta:
         model = User
         fields = ('uuid', 'username', 'email', 'image_s3_path', 'role', 'title', 'password')
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data.get('password'))
-        return super(UserSerializer, self).create(validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            role=validated_data.get('role', 'User'),
+            password=validated_data['password']
+        )
+
+        image = validated_data.get('image_s3_path')
+        if image is not None:
+            upload_to_s3(image, folder="user")
+            user.image_s3_path = f"{settings.DEFAULT_AWS_STORAGE_URL}/user/{image.name}"
+
+        user.save()
+        return user
